63e01fee1874b2791d13bec0131a0599b58fe141
Gowreeswaran <55973974+Gowreeswaran@users.noreply.github.com>
2021-11-15 14:35:05 +0530

    Update login.php

--- layouts/login.php
+++ layouts/login.php
@@ -8,7 +8,7 @@
         <div class="p-5">
             <div class="text-center">
                 <h1 class="h4 text-gray-900 mb-4">Login</h1>
-                <i class="fa fa-user-circle login-icon" aria-hidden="true"></i>
+                
             </div>
             <form name="loginForm" method="POST" action="index.php" class="user">
                 <div class="form-group">
